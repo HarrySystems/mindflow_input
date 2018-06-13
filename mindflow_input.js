910fef7a642f0046299a83427b1b30f5a2f325fd
HarrySystems <luizgdpsantos@gmail.com>
2018-06-13 16:40:15 -0300

    First commit

---
+++ mindflow_input.js
@@ -0,0 +1,14 @@
+MindFlow.node({
+	about: {
+		name: "input",
+		description: "Simple data entry for semantic pipelines"
+	},
+	
+	format: {
+		
+	},
+	
+	execute: function({ data, input }) {
+		return data
+	}
+})
